d4297e7bae86d0780bc31c7e53d4c197536f1682
Aritra Chatterjee <ach142812@gmail.com>
2024-12-09 22:29:45 +0530

    input & output

---
+++ src/main/kotlin/basics/inputoutput.kt
@@ -0,0 +1,15 @@
+package org.example.basics
+
+fun main(){
+    // Output
+    println("This is println")
+
+    // Input
+    print("Enter age: ")
+    val age = readln()
+    println("I am $age years old")
+
+    readLine()
+
+
+}
