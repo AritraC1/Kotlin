9699da3d8956a6f7c6e75ba99d66143dd8c8f5ea
Aritra Chatterjee <ach142812@gmail.com>
2024-12-09 16:59:41 +0530

    Variables and Data Types

--- src/main/kotlin/basics/variables.kt
+++ src/main/kotlin/basics/variables.kt
@@ -1,6 +1,11 @@
+// Variables and Data Types
+
 package org.example.basics
 
 fun main(){
+
+    // Type inference: Type inference in Kotlin refers to the ability of the Kotlin compiler to automatically determine the type of variable or expression based on the value it is assigned or returned from a function
+
     var name = "Tony Stark"
     println(name)
 
@@ -24,4 +29,25 @@ fun main(){
     a = 50
     b = 40
     println("After: The sum of $a and $b is ${a+b}")
+
+    // Explicitly specify the type
+    // var nameOfVariable: Data Type = Value Of Variable
+
+    var age: Int = 500
+    var fullName: String = "Thor"
+    println("I am $fullName, I am $age years old")
+
+    // Data Types
+//    val num1: Int = 1
+//    val num2: Byte = 12
+//    val num3: Short = 56
+//    val num4: Long = 4567890876543
+//    val num5: Float = 45.6f
+//    val num6: Double = 67.8
+//    val ch: Char = 'a'
+//    val truth: Boolean = true // or false
+//    val name1: String = "Steve Rogers"
+
+    // Arrays
+    // val fruits: Array<String> = arrayOf("Banana", "Mango", "Apple", "Grapes")
 }
