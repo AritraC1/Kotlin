c3a1160adcbc5f389e1ccb5da6ea9792a07b7e55
Aritra Chatterjee <ach142812@gmail.com>
2024-12-09 23:10:39 +0530

    loops

---
+++ src/main/kotlin/basics/Loops.kt
@@ -0,0 +1,39 @@
+package org.example.basics
+
+fun main(){
+    // For Loops
+    // Either use until or .. both mean the same
+
+    for (i in 0 until 10){
+        println(i)
+    }
+
+    println()
+
+    for (i in 0..10){
+        println(i)
+    }
+
+    println("For Loop End")
+    println()
+
+    // While Loops
+    var count = 1;
+    while (count<=10){
+        println("Count $count")
+        count++
+    }
+
+    println("While Loop End")
+    println()
+
+    // Do While Loops
+    var number = 1
+    do {
+        println("Number $number")
+        number++
+    }while (number<10)
+
+    println("Do-While Loop End")
+    println()
+}
